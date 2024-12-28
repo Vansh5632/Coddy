bb911c9223d1bc56a6fc40c88e4be484395657ca
Vansh5632 <vanshgilhotra8885@gmail.com>
2024-12-28 20:37:25 +0530

    API keys updated and working updated

---
+++ src/components/providers/Footer.tsx
@@ -0,0 +1,15 @@
+import {Blocks} from "lucide-react";
+import Link from "next/link";
+
+function Footer(){
+    return (
+        <footer className="relative border-t border-gray-800/50 mt-auto">
+            <div className="absolute inset-x-0 -top-px h-px bg-gradient-to-r from-transparent via-gray-900 to-transparent"/>
+            <div className="max-w-7xl mx-auto px-4 py-8">
+                <div className="flex flex-col md:flex-row">
+
+                </div>
+            </div>
+        </footer>
+    )
+}
