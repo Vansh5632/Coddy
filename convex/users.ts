a25068432ebf77df7aa407036a1ae1ea9ff72beb
Vansh5632 <vanshgilhotra8885@gmail.com>
2024-12-29 15:37:04 +0530

    Header and footer going on

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation} from "./_generated/server";
+import { mutation,query} from "./_generated/server";
 
 export const syncUser = mutation({
   args: {
@@ -24,23 +24,23 @@ export const syncUser = mutation({
   },
 });
 
-// export const getUser = query({
-//   args: { userId: v.string() },
+export const getUser = query({
+  args: { userId: v.string() },
 
-//   handler: async (ctx, args) => {
-//     if (!args.userId) return null;
+  handler: async (ctx, args) => {
+    if (!args.userId) return ({ error: "No user ID provided" });
 
-//     const user = await ctx.db
-//       .query("users")
-//       .withIndex("by_user_id")
-//       .filter((q) => q.eq(q.field("userId"), args.userId))
-//       .first();
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .first();
 
-//     if (!user) return null;
+    if (!user) return null;
 
-//     return user;
-//   },
-// });
+    return user;
+  },
+});  
 
 // export const upgradeToPro = mutation({
 //   args: {
