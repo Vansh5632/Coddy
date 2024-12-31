61d9ce8668f72ffa3b966bda36adb70843c09f68
Vansh5632 <vanshgilhotra8885@gmail.com>
2024-12-31 08:25:42 +0530

    Added monacoEditor and configured up

--- src/app/(root)/_components/HeaderProfileBtn.tsx
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+"use client";
+import {LoginButton }from "./LoginButton";
+import { SignedOut, UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
 export const HeaderProfileBtn = () => {
   return (
-    <div>HeaderProfileBtn</div>
-  )
-}
+    <>
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="Profile"
+            href="/profile"
+            labelIcon={<User className="size-4" />}
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+
+      <SignedOut>
+        <LoginButton />
+      </SignedOut>
+    </>
+  );
+};
