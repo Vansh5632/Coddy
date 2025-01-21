b19e317b19e8f445f2339c2a6b6b9b064b0d461c
Vansh5632 <vanshgilhotra8885@gmail.com>
2025-01-21 22:34:31 +0530

    changes

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client"
 
-const OutputPanel = () => {
-  return (
-    <div>OutputPanel</div>
+import { useCodeEditorStore } from "@/store/useCodeEditor"
+
+function OutputPanel(){
+  const {output,error,isRunning} = useCodeEditorStore();
+  const [isCopied, setIsCopied] = useState(false);
+
+  const hasContent = error|| output;
+
+  const handleCopy = async()=>{
+    if(!hasContent) return;
+    await navigator.clipboard.writeText(hasContent);
+    setIsCopied(true);
+
+    setTimeout(()=> setIsCopied(false),2000);
+  };
+
+  return(
+    <div>
+
+    </div>
   )
 }
-
-export default OutputPanel
