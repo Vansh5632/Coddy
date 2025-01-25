7749a68cc98c06020af894c4c3c41469295f2711
Vansh5632 <vanshgilhotra8885@gmail.com>
2025-01-25 15:51:39 +0530

    code executions working sucessfully

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import Header from  "./_components/Header";
 import Editor from "./_components/EditorPanel";
 import OutputPanel from "./_components/OutputPanel";
+import { SignIn, SignInButton } from "@clerk/nextjs";
 
 export default function Home(){
     return (
         <div className="min-h-screen">
             <div className="max-w-[1800px] mx-auto p-4">
                 <Header/>
+                <SignInButton/>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                     <Editor/>
                     <OutputPanel/>
