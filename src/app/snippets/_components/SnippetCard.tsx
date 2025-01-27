6a220e88da3d6f084c61bf1921899703f230dd52
Vansh5632 <vanshgilhotra8885@gmail.com>
2025-01-27 23:56:18 +0530

    snippet card

--- src/app/snippets/_components/SnippetCard.tsx
+++ src/app/snippets/_components/SnippetCard.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { Snippet } from '@/types'
 import React from 'react'
+import { useUser } from '@clerk/nextjs';
+import { useMutation } from 'convex/react';
+import { useState } from 'react';
+import {motion} from 'framer-motion'
+import Link from 'next/link';
+import { api } from '../../../convex/_generated/api';
+import 
 
-export const SnippetCard = () => {
+
+export const SnippetCard = ({snippet}:{snippet:Snippet}) => {
   return (
-    <div>e</div>
+    <div></div>
   )
 }
