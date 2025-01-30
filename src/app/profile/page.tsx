c9fb04e7f99b983562611dac998c99d6ff7a0772
Vansh5632 <vanshgilhotra8885@gmail.com>
2025-01-30 22:14:54 +0530

    paginated query

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,4 +13,42 @@ import Image from "next/image";
 import Link from "next/link";
 import { StarButton } from "@/components/StarButton";
 import CodeBlock from "../snippets/[id]/_components/CodeBlock";
+import { label } from "framer-motion/client";
+import { use } from "framer-motion/m";
 
+const TABS = [
+    {
+        id:"executions",
+        label:"Code Executions",
+        icon:ListVideo,
+    },
+    {
+        id:"starred",
+        label:"Starred Snippets",
+        icon:Star,
+    },
+];
+
+const export ProfilePage=()=>{
+    const {user,isLoaded} = useuser();
+    const router = useRouter();
+    const [activeTab,setActiveTab] = useState<"executions"|"starred">("executions");
+
+    const userStats = useQuery(api.codeExecutions.getUserStats,{
+        userId:user?.id??"",
+    });
+
+    const starredSnippets = useQuery(api.snippets.getStarredSnippets);
+
+    const {
+        results:executions,
+        status:executionStatus,
+        isLoading:isLoadingExecutions,
+        loadMore,
+    } = usePaginatedQuery(api.codeExecutions.getUserExecutions,{
+        userId:user?.id??"",
+    },
+{
+    initialNumItems:5
+})
+}
